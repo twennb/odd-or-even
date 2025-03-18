0ebe834662b887922815a1d8c268f357b183b8c5
twennb <tom.wennberg@hotmail.com>
2025-03-18 15:00:50 +0100

    Initial commit with program plan and main function

---
+++ oddoreven.py
@@ -0,0 +1,14 @@
+"""Program evaluates number input for odd or even"""
+# plan:
+# prompt user for input,
+# evaluate input % 2 == 0,
+# return odd or even
+# ask to take another number or exit
+
+
+def main():
+    """the main function"""
+
+
+if __name__ == "__main__":
+    main()
