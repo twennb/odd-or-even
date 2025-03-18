19d0d883255e39f86be72c82bee8c5aa56c3f51e
twennb <tom.wennberg@hotmail.com>
2025-03-18 15:12:29 +0100

    Added a function for prompting user to go again, if not it quits the application

--- oddoreven.py
+++ oddoreven.py
@@ -5,6 +5,8 @@
 # return odd or even
 # ask to take another number or exit
 
+import sys
+
 
 def odd_or_even(num):
     """evaluates and returns if num is odd or even"""
@@ -16,20 +18,37 @@ def odd_or_even(num):
         return False
 
 
+def go_again():
+    """function checks if user wants to keep going or exit"""
+    while True:
+        choice = input(
+            "\nDo you want to check another number or stop here? (yes/no)\n-: ").strip().lower()
+
+        if choice == "yes":
+            break
+        elif choice == "no":
+            sys.exit()
+        else:
+            print("Please enter 'yes' or 'no'.\n")
+
+
 def main():
     """the main function"""
     while True:
-        try:
-            num = int(
-                input("\nPlease enter which number I should check for odd or even!\n-: "))
-            break
-        except ValueError:
-            print("Invalid input! Please enter a number")
+        while True:
+            try:
+                num = int(
+                    input("\nPlease enter which number I should check for odd or even!\n-: "))
+                break
+            except ValueError:
+                print("Invalid input! Please enter a number")
 
-    if odd_or_even(num):
-        print("Even!")
-    else:
-        print("Odd!")
+        if odd_or_even(num):
+            print("Even!")
+        else:
+            print("Odd!")
+
+        go_again()
 
 
 if __name__ == "__main__":
