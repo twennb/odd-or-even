d4b92446d672bb70d000b14fa0a469aa1b50de35
twennb <tom.wennberg@hotmail.com>
2025-03-18 15:06:42 +0100

    Added function and user prompting to check a number for odd or even

--- oddoreven.py
+++ oddoreven.py
@@ -6,8 +6,30 @@
 # ask to take another number or exit
 
 
+def odd_or_even(num):
+    """evaluates and returns if num is odd or even"""
+    # num: the user's number
+
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
 def main():
     """the main function"""
+    while True:
+        try:
+            num = int(
+                input("\nPlease enter which number I should check for odd or even!\n-: "))
+            break
+        except ValueError:
+            print("Invalid input! Please enter a number")
+
+    if odd_or_even(num):
+        print("Even!")
+    else:
+        print("Odd!")
 
 
 if __name__ == "__main__":
